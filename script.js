42d2e017d0aa5b3bf3a59482703b33fe4aaab9d4
Artem <blekart04@mail.ru>
2023-12-01 23:52:31 +0300

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  document.body.style.overflow = "auto";
+  document.body.classList.add("loaded_hiding");
+  window.setTimeout(function () {
+    document.body.classList.add("loaded");
+    document.body.classList.remove("loaded_hiding");
+  }, 500);
+};
